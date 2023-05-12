fafa8688c828ebc59f1e300d5172e7a751a0666a
victor.trajano <victor.dasilva@consulting-for.edenred.com>
2023-05-12 17:01:26 -0300

    upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,45 @@
-import Button from '@material-ui/core/Button';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from '@mui/material/Button';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 import './App.css';
 
-import img1 from './assets/img/foto 1.jpg';
-import img2 from './assets/img/foto 2.jpg';
-import img3 from './assets/img/foto 3.jpg';
-import img4 from './assets/img/foto 4.jpg';
-import img10 from './assets/img/foto10.jpg';
-import img13 from './assets/img/foto13.jpg';
+import { ListSubheader } from '@mui/material';
+import img1 from './assets/img/foto1.jpg';
+import img2 from './assets/img/foto2.jpg';
+import img3 from './assets/img/foto3.jpg';
+import img4 from './assets/img/foto4 (2).jpg';
+import img5 from './assets/img/foto5.jpg';
 import img6 from './assets/img/foto6.jpg';
 import img7 from './assets/img/foto7.jpg';
 import img8 from './assets/img/foto8.jpg';
 import img9 from './assets/img/foto9.jpg';
+import img10 from './assets/img/foto10.jpg';
+import img11 from './assets/img/foto11.jpg';
+import img12 from './assets/img/foto12.jpg';
+import img14 from './assets/img/foto14.jpg';
+import img15 from './assets/img/foto15.jpg';
+import img16 from './assets/img/foto16.jpg';
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
   },
   imageList: {
-    width: 700,
-    height: 650,
+    width: 1200,
+    height: 850,
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
   },
 }));
 
-const itemData1 = [
+const itemData = [
   {
     img: img1,
   },
@@ -56,7 +61,7 @@ const itemData2 = [
     img: img6,
   },
   {
-    img: img13,
+    img: img11,
   }
 ]
 
@@ -78,6 +83,15 @@ const itemData4 = [
   }
 ]
 
+const itemData5 = [
+  {
+    img: img5,
+  },
+  {
+    img: img16,
+  }
+]
+
 function App() {
 
   const classes = useStyles();
@@ -89,7 +103,6 @@ function App() {
   }
 
   return (
-
     <>
       {
         !activeGallery && (
@@ -109,50 +122,78 @@ function App() {
           </Button>
         }
       </div>
-      <div className={classes.root}>
-        <ImageList rowHeight={300} className={classes.imageList}>
-          <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <h2>Tudo começou lá trás em 2017 </h2>
+      <div className='imageContainer'>
+
+        <ImageList sx={{ width: 700, height: 650 }} variant="quilted" cols={2} gap={5}>
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Tudo começou em 2017...</ListSubheader>
           </ImageListItem>
-          {itemData1.map((item) => (
+          {itemData.map((item) => (
             <ImageListItem key={item.img}>
-              <img src={item.img} />
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
             </ImageListItem>
           ))}
 
-          <ImageListItem key="Subheader" cols={2} style={{ height: 'auto', textAlign: 'center' }}>
-            <h2>Até que eu te reencontrei </h2>
-          </ImageListItem >
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Até que nos reecontramos...</ListSubheader>
+          </ImageListItem>
           {itemData2.map((item) => (
-
             <ImageListItem key={item.img}>
-              <img src={item.img} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }} />
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
             </ImageListItem>
           ))}
 
-          <ImageListItem key="Subheader" cols={2} style={{ height: 'auto', textAlign: 'center' }}>
-            <h2>Veio toda malhada, dlc</h2>
-          </ImageListItem >
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Você chegou malhada toda dlc </ListSubheader>
+          </ImageListItem>
           {itemData3.map((item) => (
-
             <ImageListItem key={item.img}>
-              <img src={item.img} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }} />
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
             </ImageListItem>
           ))}
 
-          <ImageListItem key="Subheader" cols={2} style={{ height: 'auto', textAlign: 'center' }}>
-            <h2>Me fazer sorrir todos os dias 😃</h2>
-          </ImageListItem >
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Me fazendo sorrir todos os dias 😁</ListSubheader>
+          </ImageListItem>
           {itemData4.map((item) => (
-
             <ImageListItem key={item.img}>
-              <img src={item.img} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }} />
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Você não é palhaça não. Só me viciou em voce, em nós.</ListSubheader>
+          </ImageListItem>
+          {itemData5.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=161&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
             </ImageListItem>
           ))}
         </ImageList>
       </div>
       <Button variant="contained" color="primary">
-        <a href="https://api.whatsapp.com/send?phone=5592982127867&text=te%20amo,%20voc%C3%AA%20%C3%A9%20o%20amor%20da%20minha%20vida%20%E2%9D%A4%EF%B8%8F">Press and send the message to enable access and continue...</a> 
+        <a href="https://api.whatsapp.com/send?phone=5592982127867&text=te%20amo,%20voc%C3%AA%20%C3%A9%20o%20amor%20da%20minha%20vida%20%E2%9D%A4%EF%B8%8F">Press and send the message to enable access and continue...</a>
       </Button>
     </>
   )
